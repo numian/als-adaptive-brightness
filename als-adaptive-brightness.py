631574406da43e206508fd4d538ed9bddb5e8031
Marco Santos <me@marcosantos.me>
2014-08-09 23:54:40 +0100

    Added some arguments validation

--- als-adaptive-brightness.py
+++ als-adaptive-brightness.py
@@ -5,12 +5,22 @@ from lib import als, brightness
 
 import argparse
 
+def checkPercentage(value):
+
+    int_value = int(value)
+    
+    if int_value < 0 or int_value > 100:
+        raise argparse.ArgumentTypeError('%s is an invalid percentage. Must be between 0 and 100.' % value)
+        
+    return int_value
+
 
 parser = argparse.ArgumentParser(description='Adapts backlight to Ambient Light Sensor value')
 parser.add_argument('--max-als-value', help='Value from ALS above which the screen brightness will be 100%%', default=300, type=int)
-parser.add_argument('--min-brightness', help='The minimum brightness percentage', default=15, type=int)
+parser.add_argument('--min-brightness', help='The minimum brightness percentage', default=15, type=checkPercentage)
 
 results = parser.parse_args()
+
 
 MAX_ALS_VALUE = results.max_als_value
 MIN_BRIGHTNESS_VALUE = results.min_brightness
@@ -35,4 +45,3 @@ def setBrightness(ali_value):
 
 if __name__ == '__main__':
     als.startReading(setBrightness)
-

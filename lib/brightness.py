4ac217c32d56c12feebd4f5dc6f47da3a50559bd
Marco Santos <me@marcosantos.me>
2014-08-09 23:41:04 +0100

    Don't call xbrightness if brightness don't change

--- lib/brightness.py
+++ lib/brightness.py
@@ -3,10 +3,16 @@
 
 import os
 
-
 XB_TIME  = 1000
 XB_STEPS = 50
 
+current_brightness = False
+
 def set(percentage):
-    os.system('xbacklight -time %i -steps %i -set %i' % (XB_TIME, XB_STEPS, percentage) )
+
+    global current_brightness
+
+    if current_brightness == False or current_brightness != percentage:
+        os.system('xbacklight -time %i -steps %i -set %i' % (XB_TIME, XB_STEPS, percentage) )
+        current_brightness = percentage
 
